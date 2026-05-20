e31ac81d16c1d798de47458a4aed3288fa67e729
igor <igorbernardorocha@gmail.com>
2026-05-20 18:32:51 -0300

    teste

--- Exc - 00/index.php
+++ Exc - 00/index.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Me livrando da maldicao</title>
+    <title>Me livrando da maldição</title>
 </head>
 <body>
+
     <h1>
         <?php
-        echo "Ola, mundo! \u{1F30E}"; // \U{} botar emoji
+            echo "Olá, mundo! 🌎";
         ?>
     </h1>
-    <p>Vamos tentar nos livrar da maldicao</p>
+
+    <p>Vamos tentar nos livrar da maldição</p>
+
 </body>
 </html>
